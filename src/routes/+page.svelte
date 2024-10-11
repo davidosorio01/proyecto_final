f970fb951076cc882a848700c979dc56c78f82f9
David Osorio <davidoso7901@gmail.com>
2024-10-11 11:49:46 -0500

    CONFIRMACION

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,14 @@
     let btnSignUp 
     let signIn = false
     import '$lib/CSS/inicio_sesion.css'
+    import Atalanta from '$lib/IMAGES/Atlanta.webp'
 </script>
+
+<svelte:head>
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</svelte:head>
 
 <div class="container" class:toggle={signIn}>
     <div class="container-form">
