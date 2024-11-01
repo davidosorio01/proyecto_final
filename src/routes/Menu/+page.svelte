24be74743514a8fe5fc54f5202dce0dc4ece21f7
David Osorio <davidoso7901@gmail.com>
2024-11-01 18:15:31 -0500

    sss

--- src/routes/Menu/+page.svelte
+++ src/routes/Menu/+page.svelte
@@ -18,12 +18,33 @@
 	import '$lib/CSS/Menu.css';
 	import Swal from 'sweetalert2';
 
-	Swal.fire({
-		title: "¡Bienvenido!",
-		text: 'Ya puedes disfrutar de nuestra página web. Recuerda que en 30 días, debes iniciar sesión de nuevo.',  
-  		icon: 'success',  
-  		confirmButtonText: 'Aceptar'
-	});
+	// Función para mostrar el modal
+	function showWelcomeModal() {
+		// Verificar si el modal ya fue mostrado
+		if (!localStorage.getItem('modalShown')) {
+			Swal.fire({
+				title: '¡Bienvenido!',
+				text: 'Ya puedes disfrutar de nuestra página web. Recuerda que en 30 días, debes iniciar sesión de nuevo.',
+				icon: 'success',
+				confirmButtonText: 'Aceptar'
+			}).then(() => {
+				// Al confirmar, almacena en localStorage que se ha visto el modal
+				localStorage.setItem('modalShown', 'true');
+			});
+		}
+	}
+	// Llamar a la función al iniciar el componente (cuando se monta)
+	showWelcomeModal();
+
+    // Función para mostrar el modal de inicio de sesión
+    function showLoginModal() {
+        Swal.fire({
+            title: '¡Ya estás vinculado!',
+            text: 'No necesitas iniciar sesión nuevamente.',
+            icon: 'info',
+            confirmButtonText: 'Continuar',
+        });
+    }
 
 	let currentSlide = 0;
 	const slides = [
@@ -104,8 +125,12 @@
 							<li><a href="#Contactanos">Contactanos</a></li>
 						</ul>
 					</nav>
-					<a href="/" class="btn__quote" data-sveltekit-preload-data="tap" data-sveltekit-reload
-						>INICIAR SESIÓN</a
+					<a
+						href="/"
+						on:click|preventDefault={showLoginModal}
+						class="btn__quote"
+						data-sveltekit-preload-data="tap"
+						data-sveltekit-reload>INICIAR SESIÓN</a
 					>
 				</div>
 			</div>
@@ -198,10 +223,9 @@
 			<div class="terms">
 				<p>
 					En SS somos un equipo de apasionados por el deporte que creemos en el poder que este tiene
-					para unir a las personas y crear momentos inolvidables.
-					Nuestra misión es brindar a los fanáticos del deporte una plataforma completa y confiable
-					donde puedan encontrar toda la información que necesitan para estar al día con sus equipos
-					y jugadores favoritos.
+					para unir a las personas y crear momentos inolvidables. Nuestra misión es brindar a los
+					fanáticos del deporte una plataforma completa y confiable donde puedan encontrar toda la
+					información que necesitan para estar al día con sus equipos y jugadores favoritos.
 				</p>
 			</div>
 		</div>
@@ -212,13 +236,31 @@
 			<a href="https://www.nba.com/">Basquetbol</a>
 			<a href="https://www.google.com/search?q=Tenis+deporte">Tenis</a>
 		</div>
-		<div class="box__footer"><!--Contenedor de nuestros contactos-->
+		<div class="box__footer">
+			<!--Contenedor de nuestros contactos-->
 			<h2>Redes Sociales</h2>
-			<a href="https://x.com/sportscores2024"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg> Twitter</a>
-			<a href="https://www.tiktok.com/@sport.score"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M448 209.9a210.1 210.1 0 0 1 -122.8-39.3V349.4A162.6 162.6 0 1 1 185 188.3V278.2a74.6 74.6 0 1 0 52.2 71.2V0l88 0a121.2 121.2 0 0 0 1.9 22.2h0A122.2 122.2 0 0 0 381 102.4a121.4 121.4 0 0 0 67 20.1z"/></svg>Tik Tok</a>
-			<a href="https://www.instagram.com/sport_scores24/"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg> Instagram</a>
+			<a href="https://x.com/sportscores2024"
+				><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+					><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+						d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"
+					/></svg
+				> Twitter</a
+			>
+			<a href="https://www.tiktok.com/@sport.score"
+				><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
+					><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+						d="M448 209.9a210.1 210.1 0 0 1 -122.8-39.3V349.4A162.6 162.6 0 1 1 185 188.3V278.2a74.6 74.6 0 1 0 52.2 71.2V0l88 0a121.2 121.2 0 0 0 1.9 22.2h0A122.2 122.2 0 0 0 381 102.4a121.4 121.4 0 0 0 67 20.1z"
+					/></svg
+				>Tik Tok</a
+			>
+			<a href="https://www.instagram.com/sport_scores24/"
+				><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
+					><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+						d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+					/></svg
+				> Instagram</a
+			>
 		</div>
-
 	</div>
 	<div class="box__copyright">
 		<hr />
